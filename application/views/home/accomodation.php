ee4844935658c75c78460c426d303c9ba525112e
Steven Lee <hendrawanp13bintan@gmail.com>
2017-12-01 20:55:41 +0700

    accommodation

--- application/views/home/accomodation.php
+++ application/views/home/accomodation.php
@@ -2,7 +2,7 @@
 	<div class="col-md-12">
 		<div class="col-md-10 col-md-offset-1" style="margin-top:2%; margin-bottom: 8%;">
 			<h1>Accommodation</h1>
-			<div class="col-md-3">
+			<div class="col-md-4">
 				<center><div style="width: auto; height: auto;">
 					<img style="width: 100%" src="<?php echo base_url();?>/assets/images/kost.png">
 				</div></center>
@@ -10,7 +10,7 @@
 					 <h4 class="top20 text-justify font-red"><h3>Kost U 18 A</h3></h4>
 					<p class="text-justify">
 						<ul class="text-justify" type="disc">
-							<li>AC, kamar mandi dalam, Wifi, Cewe/Cowo </li>
+							<li>AC, kamar mandi dalam , Wifi, Man/ Woman </li>
 							<li>Rp. 100.000 </li>
 							<li>Yanto - 087878392089 </li>
 							<li>jln U no 18A kemanggisan, Jakarta Barat, DKI Jakarta 11480</li>
@@ -19,7 +19,7 @@
 				</div>
 			</div>
 
-			<div class="col-md-3">
+			<div class="col-md-4">
 				<center><div style="width: auto; height: auto;">
 					<img style="width: 100%" src="<?php echo base_url();?>/assets/images/kost.png">
 				</div></center>
@@ -27,16 +27,16 @@
 					 <h4 class="top20 text-justify font-red"><h3>ICON</h3></h4>
 					<p class="text-justify">
 						<ul class="text-justify" type="disc">
-							<li>AC, kamar mandi dalam, Wifi, Cewe/Cowo </li>
+							<li>AC, Kamar mandi dalam,  Wifi, Man/ Woman</li>
 							<li>Rp. 125.000 </li>
 							<li>Yanto - 087878392089 </li>
-							<li>Jl. Kb. Jeruk Raya No.68, RT.9/RW.15, Palmerah, Jakarta Barat, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11480</li>
+							<li>Jl. Kb. Jeruk Raya No.68 (Depan Binus Anggrek), RT.9/RW.15, Palmerah, Jakarta Barat, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11480</li>
 						</ul>
 					</p>
 				</div>
 			</div>
 
-			<div class="col-md-3">
+			<div class="col-md-4">
 				<center><div style="width: auto; height: auto;">
 					<img style="width: 100%" src="<?php echo base_url();?>/assets/images/kost.png">
 				</div></center>
@@ -44,7 +44,7 @@
 					 <h4 class="top20 text-justify font-red"><h3>Kost The Helena Home</h3></h4>
 					<p class="text-justify">
 						<ul class="text-justify" type="disc">
-							<li>AC, kamar mandi dalam , wifi  </li>
+							<li>AC, kamar mandi dalam , wifi , Man/ Woman</li>
 							<li>Rp. 150.000 </li>
 							<li>Mba siti - 081510991803 </li>
 							<li>Blok D No.7, Jl. Rw. Belong No.2, RT.1/RW.11, Palmerah, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11480 </li>
@@ -53,7 +53,11 @@
 				</div>
 			</div>
 
-			<div class="col-md-3">
+	</div>
+
+	<div class="col-md-10 col-md-offset-1">
+
+			<div class="col-md-4">
 				<center><div style="width: auto; height: auto;">
 					<img style="width: 100%" src="<?php echo base_url();?>/assets/images/kost.png">
 				</div></center>
@@ -61,7 +65,7 @@
 					 <h4 class="top20 text-justify font-red"><h3>Kost C3</h3></h4>
 					<p class="text-justify">
 						<ul class="text-justify" type="disc">
-							<li>Ac, kamar mandi luar , wifi  </li>
+							<li>AC, kaman mandi luar  , wifi, Man/ Woman</li>
 							<li>Rp. 100.000 </li>
 							<li>Kusdi - 083813712965 </li>
 							<li>Jl. Komplek Sandang no C3 Palmerah, Jakarta Barat</li>
@@ -69,28 +73,25 @@
 					</p>
 				</div>
 			</div>
-	</div>
 
-	<div class="col-md-10 col-md-offset-1">
-
-			<div class="col-md-3">
+			<div class="col-md-4">
 				<center><div style="width: auto; height: auto;">
 					<img style="width: 100%" src="<?php echo base_url();?>/assets/images/kost.png">
 				</div></center>
 				<div style="background-color: rgba(255,255,255,1);">
-					 <h4 class="top20 text-justify font-red"><h3>Kost Melati</h3></h4>
+					 <h4 class="top20 text-justify font-red"><h3>Kost U30</h3></h4>
 					<p class="text-justify">
 						<ul class="text-justify" type="disc">
-							<li>cewe/cowo, kamar mandi luar, no ac</li>
-							<li>Rp. 50.000 </li>
-							<li>021 - 5306441 / 021 - 5306470</li>
-							<li>ALAMAT</li>
+							<li>AC, listrik, kamar mandi dalam, laundry, Man/Woman</li>
+							<li>Price not available</li>
+							<li>Patricia - 087888988808</li>
+							<li>Gang U</li>
 						</ul>
 					</p>
 				</div>
 			</div>
 
-			<div class="col-md-3">
+			<div class="col-md-4">
 				<center><div style="width: auto; height: auto;">
 					<img style="width: 100%" src="<?php echo base_url();?>/assets/images/kost.png">
 				</div></center>
@@ -98,7 +99,7 @@
 					 <h4 class="top20 text-justify font-red"><h3>Kost Inter 9</h3></h4>
 					<p class="text-justify">
 						<ul class="text-justify" type="disc">
-							<li>wifi, kamar mandi luar, cewe/cowo </li>
+							<li>AC,wifi, Kamar mandi luar , Man/woman</li>
 							<li>Rp. 150.000 </li>
 							<li>081586988886</li>
 							<li>Jl. Rw. Belong No.9, RT.1/RW.9, Kb. Jeruk, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11530</li>
@@ -106,8 +107,11 @@
 					</p>
 				</div>
 			</div>
+		</div>
+	</div>
 
-			<div class="col-md-3">
+	<div class="col-md-10 col-md-offset-1">
+		<div class="col-md-4">
 				<center><div style="width: auto; height: auto;">
 					<img style="width: 100%" src="<?php echo base_url();?>/assets/images/kost.png">
 				</div></center>
@@ -115,16 +119,16 @@
 					 <h4 class="top20 text-justify font-red"><h3>Kost Gloria</h3></h4>
 					<p class="text-justify">
 						<ul class="text-justify" type="disc">
-							<li>listik, kamar mandi dalam dan luar, wifi, khusus cewe</li>
+							<li>AC, listrik, kamar mandi luar , wifi, woman only</li>
 							<li>Rp. 100.000 </li>
 							<li>Rian - 081298458415</li>
-							<li>ALAMAT</li>
+							<li>Gang U </li>
 						</ul>
 					</p>
 				</div>
 			</div>
 
-			<div class="col-md-3">
+			<div class="col-md-4">
 				<center><div style="width: auto; height: auto;">
 					<img style="width: 100%" src="<?php echo base_url();?>/assets/images/kost.png">
 				</div></center>
@@ -132,7 +136,7 @@
 					 <h4 class="top20 text-justify font-red"><h3 >Kost U4</h3></h4>
 					<p class="text-justify">
 						<ul class="text-justify" type="disc">
-							<li>AC, listrik, kamar mandi luar, wifi, laundry , khusus cewe</li>
+							<li>AC, listrik, Kamar mandi luar, wifi, laundry , woman only</li>
 							<li>Rp. 100.000 </li>
 							<li>Shinta - 083807012886</li>
 							<li>Jl. U No. 4 Palmerah, Jakarta, Jakarta Barat 11480, Indonesia</li>
@@ -140,6 +144,22 @@
 					</p>
 				</div>
 			</div>
-		</div>
+
+			<div class="col-md-4">
+				<center><div style="width: auto; height: auto;">
+					<img style="width: 100%" src="<?php echo base_url();?>/assets/images/kost.png">
+				</div></center>
+				<div style="background-color: rgba(255,255,255,1);">
+					 <h4 class="top20 text-justify font-red"><h3 >Kost Mangga</h3></h4>
+					<p class="text-justify">
+						<ul class="text-justify" type="disc">
+							<li>No wifi, kamar mandi dalam, Man/Woman</li>
+							<li>Rp. 300.000 </li>
+							<li>081322248989</li>
+							<li>Jl. KH.Syahdan </li>
+						</ul>
+					</p>
+				</div>
+			</div>
 	</div>
 </div>
